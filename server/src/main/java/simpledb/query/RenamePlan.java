0c7ae6ee948f403c05e0d7509dfd07dac141d423
Ashleigh Bunkofske <bunkas01@luther.edu>
2017-12-08 12:04:16 -0600

    Finished initial implementation of RenamePlan/Scan, added files for SemiJoinPlan/Scan.

--- server/src/main/java/simpledb/query/RenamePlan.java
+++ server/src/main/java/simpledb/query/RenamePlan.java
@@ -5,10 +5,57 @@
  */
 package simpledb.query;
 
-///**
-// *
-// * @author Ashleigh
-// */
-//public class RenamePlan implements Plan {
-//    
-//}
+import java.util.Collection;
+import simpledb.record.Schema;
+
+/**
+ *
+ * @author Ashleigh
+ */
+public class RenamePlan implements Plan {
+    private Plan p;
+    private Schema schema = new Schema();
+    private String oldName, newName;
+    private Collection<String> oldFields;
+    
+    public RenamePlan(Plan p, String oldName, String newName) {
+        this.p = p;
+        Schema old = p.schema();
+        Collection<String> fieldlist = old.fields();
+        oldFields = fieldlist;
+        for (String fldname : fieldlist) {
+            if (fldname == oldName) {
+                schema.addField(newName, old.type(oldName), old.length(oldName));
+            } else {
+                schema.add(fldname, old);
+            }
+        }
+        this.oldName = oldName;
+        this.newName = newName;
+    }
+    
+    public Scan open() {
+        Scan s = p.open();
+        return new RenameScan(s, schema.fields(), oldName, newName);
+    }
+    
+    public int blocksAccessed() {
+        return p.blocksAccessed();
+    }
+    
+    public int recordsOutput() {
+        return p.recordsOutput();
+    }
+    
+    public int distinctValues(String fldname) {
+        if (fldname == newName) {
+            return p.distinctValues(oldName);
+        } else {
+            return p.distinctValues(fldname);
+        }
+    }
+    
+    public Schema schema() {
+        return schema;
+    }
+}
--- server/src/main/java/simpledb/query/RenameScan.java
+++ server/src/main/java/simpledb/query/RenameScan.java
@@ -5,10 +5,74 @@
  */
 package simpledb.query;
 
+import java.util.Collection;
+
 /**
  *
  * @author Ashleigh
  */
-//public class RenameScan implements Scan {
-//    
-//}
+public class RenameScan implements Scan {
+    private Scan s;
+    private Collection<String> fieldlist;
+    private String oldName, newName;
+    
+    public RenameScan(Scan s, Collection<String> fieldlist,
+                      String old, String newName) {
+        this.s = s;
+        this.fieldlist = fieldlist;
+        this.oldName = old;
+        this.newName = newName;
+    }
+    
+    public void beforeFirst() {
+        s.beforeFirst();
+    }
+    
+    public boolean next() {
+        return s.next();
+    }
+    
+    public void close() {
+        s.close();
+    }
+    
+    public Constant getVal(String fldname) {
+        if (hasField(fldname)) {
+            if (fldname == newName) {
+                return s.getVal(oldName);
+            } else {
+                return s.getVal(fldname);
+            }
+        } else {
+            throw new RuntimeException("field " + fldname + " not found.");
+        }
+    }
+    
+    public int getInt(String fldname) {
+        if (hasField(fldname)) {
+            if (fldname == newName) {
+                return s.getInt(oldName);
+            } else {
+                return s.getInt(fldname);
+            }
+        } else {
+            throw new RuntimeException("field " + fldname + " not found.");
+        }
+    }
+    
+    public String getString(String fldname) {
+        if (hasField(fldname)) {
+            if (fldname == newName) {
+                return s.getString(oldName);
+            } else {
+                return s.getString(fldname);
+            }
+        } else {
+            throw new RuntimeException("field " + fldname + " not found.");
+        }
+    }
+    
+    public boolean hasField(String fldname) {
+        return fieldlist.contains(fldname);
+    }
+}
